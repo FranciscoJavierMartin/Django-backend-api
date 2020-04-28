c1103ec507e92354aac677d07f7731d4dd9405ae
Francisco <javiermartincuenca@gmail.com>
2020-04-29 01:50:19 +0200

    Fixed error on linting

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -49,7 +49,7 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.data, serializer.data)
 
     def test_ingredients_limited_to_user(self):
-        """Test that only ingredients for the authenticated user are returned"""
+        """Test that only ingredients for the authenticated user is returned"""
         user2 = get_user_model().objects.create_user(
             'other@test.com',
             'testpass'
